461b4ebe3226b5fb812b162d88206ac69ac251fb
JB-Dev <jibaoke@gmail.com>
2022-09-13 12:08:54 +0200

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import Topbar from "./components/Topbar";
-
+import SideBar from './components/SideBar';
+import Topbar from './components/Topbar';
 
 function App() {
   return (
     <div className="App">
-      <Topbar/>
+      <Topbar />
+      <div className="containerApp">
+        <SideBar />
+        <div className="others">other pages</div>
+      </div>
     </div>
   );
 }
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+
+import {LineStyle,QueryStats,TrendingUp,PermIdentity,Storefront,AttachMoney,BarChart,MailOutline,DynamicFeed,ChatBubbleOutline,WorkOutline,Report} from '@mui/icons-material';
+
+
+export default function SideBar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem active">
+              <LineStyle className="sidebarIcon" />
+              Accueil
+            </li>
+            <li className="sidebarListItem">
+              <QueryStats className="sidebarIcon" />
+              Analyse
+            </li>
+            <li className="sidebarListItem">
+              <TrendingUp className="sidebarIcon" />
+              Ventes
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Menu</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem ">
+              <PermIdentity className="sidebarIcon" />
+              Utilisateurs
+            </li>
+            <li className="sidebarListItem">
+              <Storefront className="sidebarIcon" />
+              Produits
+            </li>
+            <li className="sidebarListItem">
+              <AttachMoney className="sidebarIcon" />
+              Transactions
+            </li>
+            <li className="sidebarListItem">
+              <BarChart className="sidebarIcon" />
+              Rapports
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Notifications</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem ">
+              <MailOutline className="sidebarIcon" />
+              Mail
+            </li>
+            <li className="sidebarListItem">
+              <DynamicFeed className="sidebarIcon" />
+              Retour Mail
+            </li>
+            <li className="sidebarListItem">
+              <ChatBubbleOutline className="sidebarIcon" />
+              Messages
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Staff</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem ">
+              <WorkOutline className="sidebarIcon" />
+              Management
+            </li>
+            <li className="sidebarListItem">
+              <QueryStats className="sidebarIcon" />
+              Analyse
+            </li>
+            <li className="sidebarListItem">
+              <Report className="sidebarIcon" />
+              Rapports
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
